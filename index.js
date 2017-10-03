514405ec1882c2a0e479d632ca0dd0e374ec142f
Koki Takahashi <hakatasiloving@gmail.com>
2017-10-03 12:39:52 +0900

    Implement request assertion

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const Hapi = require('hapi');
 const generateImage = require('./generateImage.js');
 
@@ -10,8 +11,20 @@ hapi.route({
 	method: 'GET',
 	path: '/images/{pais}',
 	handler: async (request, reply) => {
+		const 牌s = Array.from(request.params.pais);
+
+		try {
+			assert(牌s.length !== 0);
+			assert(牌s.length <= 14);
+			assert(牌s.every((牌) => (
+				0x1F000 <= 牌.codePointAt(0) && 牌.codePointAt(0) <= 0x1F021
+			)));
+		} catch (error) {
+			return reply(`Bad Request: ${error.message}`).code(400);
+		}
+
 		const png = await generateImage(Array.from(request.params.pais)).catch((i) => console.log(i));
-		reply(png).type('image/png');
+		return reply(png).type('image/png');
 	},
 });
 
